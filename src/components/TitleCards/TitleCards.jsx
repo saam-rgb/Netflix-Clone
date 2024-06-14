9f5e98cdfe2cab4b46ec68f57089d2d509a25008
saam <saamsallvin@gmail.com>
2024-06-14 11:03:33 +0530

    added: title cards, footer

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data.js";
 
-const TitleCards = () => {
-  return <div className="titlecards"></div>;
+const TitleCards = ({ title, category }) => {
+  const cardsRef = useRef();
+
+  // *Function to scroll horizontally without pressing shift key
+  const handleWheel = (event) => {
+    event.preventDefault(); // *to disable default vertical scrolling
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+  return (
+    <div className="title-cards">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TitleCards;
